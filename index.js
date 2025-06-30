fe118a531c7b0a098a7fb9e945f628326fb45571
Karthik0405 <karthik515865@gmail.com>
2025-06-30 23:07:47 +0530

    edits

--- index.js
+++ index.js
@@ -44,3 +44,10 @@ document.addEventListener("DOMContentLoaded", () => {
     );
   });
 });
+
+const hamburger = document.getElementById("hamburger");
+const navList = document.getElementById("navList");
+
+hamburger.addEventListener("click", () => {
+  navList.classList.toggle("show");
+});
